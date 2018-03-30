96c8b65fc2855e7b0964d36505d446db98dabc7b
Kirill <madorinkv@gmail.com>
2018-03-30 18:26:36 +0300

    travis + eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #! /Users/mac/.nvm/versions/node/v5.2.0/bin/node
 
-import userName from "../"
+import userName from '../';
+
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${userName}!`);
